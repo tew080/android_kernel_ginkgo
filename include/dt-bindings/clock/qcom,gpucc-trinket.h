2d3ca15db64952a5233515ea3e144359de14e202
TEW404 <84270286+tew080@users.noreply.github.com>
2025-05-13 08:25:15 +0700

    Revert "clk: qcom: Enable Cloick Ramp Controller(CRC) for GFX3D clock for && UV"
    This reverts commit 31b0c5af9a7291cf4ba4bc0444335769e67c2c81.

--- include/dt-bindings/clock/qcom,gpucc-trinket.h
+++ include/dt-bindings/clock/qcom,gpucc-trinket.h
@@ -14,25 +14,21 @@
 #ifndef _DT_BINDINGS_CLK_QCOM_GPU_CC_TRINKET_H
 #define _DT_BINDINGS_CLK_QCOM_GPU_CC_TRINKET_H
 
-/* Hardware clocks */
-#define CRC_DIV_PLL0_OUT_AUX2			0
-#define CRC_DIV_PLL1_OUT_AUX2			1
-
 /* GPUCC clock registers */
-#define GPU_CC_PLL0_OUT_AUX2			2
-#define GPU_CC_PLL1_OUT_AUX2			3
-#define GPU_CC_CRC_AHB_CLK			4
-#define GPU_CC_CX_APB_CLK			5
-#define GPU_CC_CX_GFX3D_CLK			6
-#define GPU_CC_CX_GMU_CLK			7
-#define GPU_CC_CX_SNOC_DVM_CLK			8
-#define GPU_CC_CXO_AON_CLK			9
-#define GPU_CC_CXO_CLK			10
-#define GPU_CC_GMU_CLK_SRC			11
-#define GPU_CC_SLEEP_CLK			12
-#define GPU_CC_GX_GFX3D_CLK			13
-#define GPU_CC_GX_GFX3D_CLK_SRC			14
-#define GPU_CC_AHB_CLK			15
-#define GPU_CC_HLOS1_VOTE_GPU_SMMU_CLK		16
+#define GPU_CC_PLL0_OUT_AUX2			0
+#define GPU_CC_PLL1_OUT_AUX2			1
+#define GPU_CC_CRC_AHB_CLK			2
+#define GPU_CC_CX_APB_CLK			3
+#define GPU_CC_CX_GFX3D_CLK			4
+#define GPU_CC_CX_GMU_CLK			5
+#define GPU_CC_CX_SNOC_DVM_CLK			6
+#define GPU_CC_CXO_AON_CLK			7
+#define GPU_CC_CXO_CLK			8
+#define GPU_CC_GMU_CLK_SRC			9
+#define GPU_CC_SLEEP_CLK			10
+#define GPU_CC_GX_GFX3D_CLK			11
+#define GPU_CC_GX_GFX3D_CLK_SRC			12
+#define GPU_CC_AHB_CLK			13
+#define GPU_CC_HLOS1_VOTE_GPU_SMMU_CLK		14
 
 #endif
