728b53a7018ea5c4e235a3aa39e46098d02ee8fe
TEW404 <84270286+tew080@users.noreply.github.com>
2025-05-21 17:54:27 +0700

    fs: ext4: Adapt to new perf critical IRQs implementation

--- fs/ext4/mmp.c
+++ fs/ext4/mmp.c
@@ -376,7 +376,7 @@ skip:
 	/*
 	 * Start a kernel thread to update the MMP block periodically.
 	 */
-	EXT4_SB(sb)->s_mmp_tsk = kthread_run(kmmpd, mmpd_data, "kmmpd-%.*s",
+	EXT4_SB(sb)->s_mmp_tsk = kthread_run_perf_critical(cpu_perf_mask, kmmpd, mmpd_data, "kmmpd-%.*s",
 					     (int)sizeof(mmp->mmp_bdevname),
 					     bdevname(bh->b_bdev,
 						      mmp->mmp_bdevname));
