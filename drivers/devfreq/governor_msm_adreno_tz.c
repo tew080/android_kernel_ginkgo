4690dfd0efebf8c5d0568616b750d3c490bdd3a4
TEW404 <84270286+tew080@users.noreply.github.com>
2025-05-29 08:44:32 +0700

    Revert "msm-adreno-tz: Get rid of the sample lock"
    This reverts commit 06654ec1c8a85b837057b6a0f544d75ae059a5f7.

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -26,6 +26,7 @@
 #include "governor.h"
 
 static DEFINE_SPINLOCK(tz_lock);
+static DEFINE_SPINLOCK(sample_lock);
 /*
  * FLOOR is 5msec to capture up to 3 re-draws
  * per frame for 60fps content.
@@ -59,7 +60,7 @@ static DEFINE_SPINLOCK(tz_lock);
 
 static atomic_long_t suspend_time;
 static atomic_long_t suspend_start;
-static atomic_long_t acc_total, acc_relative_busy;
+static unsigned long acc_total, acc_relative_busy;
 
 /*
  * Returns GPU suspend time in millisecond.
@@ -89,13 +90,14 @@ static ssize_t gpu_load_show(struct device *dev,
 	 * This will keep the average value in sync with
 	 * with the client sampling duration.
 	 */
-	if (atomic_long_read(&acc_total))
-		sysfs_busy_perc = (atomic_long_read(&acc_relative_busy) * 100) /
-				   atomic_long_read(&acc_total);
+	spin_lock(&sample_lock);
+	if (acc_total)
+		sysfs_busy_perc = (acc_relative_busy * 100) / acc_total;
 
 	/* Reset the parameters */
-	atomic_long_set(&acc_total, 0);
-	atomic_long_set(&acc_relative_busy, 0);
+	acc_total = 0;
+	acc_relative_busy = 0;
+	spin_unlock(&sample_lock);
 	return snprintf(buf, PAGE_SIZE, "%lu\n", sysfs_busy_perc);
 }
 
@@ -134,21 +136,24 @@ static const struct device_attribute *adreno_tz_attr_list[] = {
 		NULL
 };
 
-static void compute_work_load(struct devfreq_dev_status *stats,
+void compute_work_load(struct devfreq_dev_status *stats,
 		struct devfreq_msm_adreno_tz_data *priv,
 		struct devfreq *devfreq)
 {
-	s64 busy;
+	u64 busy;
 
+	spin_lock(&sample_lock);
 	/*
 	 * Keep collecting the stats till the client
 	 * reads it. Average of all samples and reset
 	 * is done when the entry is read
 	 */
-	atomic_long_add(stats->total_time, &acc_total);
-	busy = stats->busy_time * stats->current_frequency;
+	acc_total += stats->total_time;
+	busy = (u64)stats->busy_time * stats->current_frequency;
 	do_div(busy, devfreq->profile->freq_table[0]);
-	atomic_long_add(busy, &acc_relative_busy);
+	acc_relative_busy += busy;
+
+	spin_unlock(&sample_lock);
 }
 
 /* Trap into the TrustZone, and call funcs there. */
